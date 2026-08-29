498048f82c5eff5a3a2ee01435d8c39843544dad
kaustubh-netizen0604 <kaustubhkrrisharyan@gmail.com>
2026-08-30 01:11:02 +0530

    add gitignore, update two_sum with user input, add contains duplicate solution

---
+++ arrays/contains_duplicate.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+bool contains_duplicate(vector<int>&nums){
+    sort(nums.begin(),nums.end());
+
+    for(int i=0;i<nums.size()-1;i++){
+        if(nums[i]==nums[i+1])
+        return true;
+    }
+return false;
+}
+};
+int main(){
+    Solution s;
+    cout<<"Enter the size : ";
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++){
+        cin>>nums[i];
+    }
+    cout<< s.contains_duplicate(nums)<< endl;
+    return 0;
+
+
+}
