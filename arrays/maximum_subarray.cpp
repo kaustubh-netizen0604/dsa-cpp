d5eba20afda7ab2a07797503e4cf875930e4f58d
kaustubh-netizen0604 <kaustubhkrrisharyan@gmail.com>
2026-08-31 21:44:51 +0530

    solved maximum subarray #53 - kadanes algorithm

---
+++ arrays/maximum_subarray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include<limits.h>
+using namespace std;
+class Solution {
+public:
+    int maxSubarray(vector<int>& nums) {
+        int sum=0,i,k=INT_MIN;
+        for(i=0;i<nums.size();i++){
+            sum+=nums[i];
+            k = max(k,sum);
+            if(sum<0)sum=0;  
+        }
+        return k;
+    }
+};
+int main(){
+    Solution s;
+    int n;
+    cout<<"enter size of array: ";
+    cin>>n;
+    vector<int> nums(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>nums[i];
+    }
+    cout<<"maximum sum of sub array is : ";
+   cout<< s.maxSubarray(nums)<<endl;
+    
+}
+
+
